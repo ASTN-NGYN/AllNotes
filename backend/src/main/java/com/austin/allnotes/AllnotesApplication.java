4cdea45fd0d06c8fbeefa2a977da1a1fb6c7174a
Austin Nguyen <austin11@uw.edu>
2025-12-17 17:05:55 -0800

    feat: initial springboot backend setup

---
+++ backend/src/main/java/com/austin/allnotes/AllnotesApplication.java
@@ -0,0 +1,13 @@
+package com.austin.allnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllnotesApplication.class, args);
+	}
+
+}
